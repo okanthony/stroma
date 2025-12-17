2ff5fee2e9995d4629d58e03afd11972970bf643
okanthony <degennaro.anthony@gmail.com>
2025-12-16 23:06:10 -0600

    update: auth store
    wrap all async fn logic in try/catch/finally
    log all errors
    add NETWORK_ERROR for setting user-friendly error messages if error caught in asyjnc fns
    fix signInWithOtp to set loading to false on error

--- stores/auth/index.ts
+++ stores/auth/index.ts
@@ -1,9 +1,13 @@
+// Internal
+import { supabase } from '@/clients/supabase';
+
+// External
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Session, User } from '@supabase/supabase-js';
-import { supabase } from '@/clients/supabase';
 
+// Types
 interface AuthState {
   // Track app hydrated in root layout before rendering UI
   isAppLoaded: boolean;
@@ -28,6 +32,10 @@ interface AuthState {
   getUserEmail: () => string;
 }
 
+// Constants
+const NETWORK_ERROR = "We ran into an issue and can't process your request, please try again later";
+
+// Store
 export const useAuthStore = create<AuthState>()(
   persist(
     (set, get) => ({
@@ -41,53 +49,87 @@ export const useAuthStore = create<AuthState>()(
       // Actions
       setIsAppLoaded: (isAppLoaded) => set({ isAppLoaded }),
 
-      // Supabase
+      // Actions - Supabase data
       setSession: (session) => set({ session }),
       setUser: (user) => set({ user }),
 
+      // Actions - async
       // signInWithOtp handles both sign in and sign up
       requestOTP: async (email) => {
         set({ isLoading: true, error: null });
 
-        const { error } = await supabase.auth.signInWithOtp({ email });
+        try {
+          const { error } = await supabase.auth.signInWithOtp({ email });
 
-        if (error) {
-          set({ error: error.message });
-          throw error;
+          // Request returned 200 but with error
+          if (error) {
+            console.error('Error requestOTP', `Supabase error: ${error.message}`);
+            set({ error: error.message });
+            throw error;
+          }
+        } catch (err) {
+          // All other errors
+          console.error('Caught Error requestOTP', err instanceof Error ? err.message : String(err));
+          set({ error: NETWORK_ERROR });
+          throw err;
+        } finally {
+          // Always set loading false
+          set({ isLoading: false });
         }
-        set({ isLoading: false });
       },
 
       verifyOTP: async (email, token) => {
         set({ isLoading: true, error: null });
 
-        const { data, error } = await supabase.auth.verifyOtp({
-          email,
-          token,
-          type: 'email'
-        });
-
-        if (error) {
-          set({ error: error.message, isLoading: false });
-          throw error;
-        } else if (data.session) {
-          set({
-            session: data.session,
-            user: data.session.user,
-            isLoading: false
+        try {
+          const { data, error } = await supabase.auth.verifyOtp({
+            email,
+            token,
+            type: 'email'
           });
+
+          // Request returned 200 but with error
+          if (error) {
+            console.error('Error verifyOTP', `Supabase error: ${error.message}`);
+            set({ error: error.message });
+            throw error;
+          }
+
+          if (data.session) {
+            set({
+              session: data.session,
+              user: data.session.user
+            });
+          }
+        } catch (err) {
+          // All other errors
+          console.error('Caught Error verifyOTP', err instanceof Error ? err.message : String(err));
+          set({ error: NETWORK_ERROR });
+          throw err;
+        } finally {
+          // Always set loading false
+          set({ isLoading: false });
         }
       },
 
       signOut: async () => {
         set({ isLoading: true, error: null });
 
-        const { error } = await supabase.auth.signOut();
+        try {
+          const { error } = await supabase.auth.signOut();
 
-        if (error) {
-          set({ error: error.message, isLoading: false });
-          throw error;
-        } else {
+          // Request returned 200 but with error
+          if (error) {
+            console.error('Error signOut', `Supabase error: ${error.message}`);
+            set({ error: error.message });
+            throw error;
+          }
+        } catch (err) {
+          // All other errors
+          console.error('Caught Error signOut', err instanceof Error ? err.message : String(err));
+          set({ error: NETWORK_ERROR });
+        } finally {
+          // Always set loading false
           set({ isLoading: false });
         }
       },
